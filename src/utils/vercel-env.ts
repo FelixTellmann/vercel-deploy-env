7c0737413733285d10ee4053b244c6f42136b7d5
Felix <felix@tellmann.co.za>
2021-10-26 14:28:30 +0200

    first commit

--- src/utils/vercel-env.ts
+++ src/utils/vercel-env.ts
@@ -5,17 +5,21 @@ import type { DeploymentEnv, EnvMap } from "../types/shared";
 
 const removeEnv = async (deploymentEnv: DeploymentEnv, envMap: EnvMap) => {
   for (const varName in envMap) {
+    const startTime = Date.now();
     exec(`vercel env rm ${varName} ${deploymentEnv} -y`).then(printStdout);
-    await delay(500);
+    await delay(650);
+    console.log(`${Date.now() - startTime}ms`);
   }
 };
 
 const addEnv = async (deploymentEnv: DeploymentEnv, envMap: EnvMap) => {
   for (const varName in envMap) {
+    const startTime = Date.now();
     exec(`printf %s "${envMap[varName]}" | vercel env add ${varName} ${deploymentEnv}`).then(
       printStdout
     );
-    await delay(500);
+    await delay(650);
+    console.log(`${Date.now() - startTime}ms`);
   }
 };
 
